6f361c265765ef01c036baab8a1cbf8b38d367ac
gabrielgoncalves95 <gabriell.gonsalves95@gmail.com>
2019-05-19 11:26:44 -0300

    Initial commit

---
+++ src/main/java/com/restaurants/demo/FastFoodTestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.restaurants.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodTestSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastFoodTestSpringApplication.class, args);
+    }
+
+}
